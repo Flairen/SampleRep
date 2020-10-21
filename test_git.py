d24260b8bb44f5f1d0e10fc2b19cf7a687de0199
Aleksey <mail@mail.ru>
2020-10-22 00:50:45 +0300

    add func 'summ'

--- test_git.py
+++ test_git.py
@@ -1,2 +1,14 @@
-if __name__ == '__main__':
+def simple_print():
     print('Hello!')
+
+
+def summ(*args):
+    s = 0
+    for arg in args:
+        s += arg
+    return s
+
+if __name__ == '__main__':
+    simple_print()
+    print(summ(5, 7, 8))
+
